3423db394ed29b9cca19d7ddcbd968e5b999cd0a
MartKaal <48913272+MartKaal@users.noreply.github.com>
2020-08-17 15:12:44 +0200

    React setup

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Test</h1>
+            <p>Nunito Sans</p>
+        </div>
+    );
+}
+
+export default App;
